8bc6f438f1cba21d8d3d70064441115736b5ac1e
keunji <keunji0708@naver.com>
2021-06-12 13:54:42 +0900

    update code

--- mysite/titanic/models/service.py
+++ mysite/titanic/models/service.py
@@ -1,1 +1,16 @@
-# 데이터 분석
+from mysite.titanic.models.dataset import Dataset
+import pandas as pd
+import numpy as np
+
+
+class Service(object):
+
+    dataset = Dataset()
+
+    def new_model(self, payload) -> object:
+        this = self.dataset
+        this.context = './data'
+        this.fname = payload    # 외부에서 입력된 값(파일명, 검색어...)
+        return pd.read_csv(this.context + this.fname)
+
+
